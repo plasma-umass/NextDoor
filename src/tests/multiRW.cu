7fccb8f418e3ec6136f3cbd46b1337f9c95c8388
Abhinav Jangda <abhijangda@gmail.com>
2021-02-05 14:13:07 -0500

    add multiRW and subgraph sampling

---
+++ src/tests/multiRW.cu
@@ -0,0 +1,292 @@
+#include "testBase.h"
+
+#include <stdlib.h>
+
+#define NUM_ROOT_VERTICES 100
+
+class MultiRWSample
+{
+public:
+  int rootVertices[NUM_ROOT_VERTICES];
+  int lastRootIdx;
+};
+
+__host__ __device__ int steps() {return 100;}
+
+__host__ __device__ 
+int stepSize(int k) {
+  return 1;
+}
+
+template<class SampleType>
+__device__ inline
+VertexID next(int step,CSRPartition* csr, const VertexID* transit, const VertexID sampleIdx,
+              SampleType* sample, 
+              const float max_weight,
+              const CSR::Edge* transitEdges, const float* transitEdgeWeights,
+              const EdgePos_t numEdges, const EdgePos_t neighbrID, curandState* state)
+{
+  if (numEdges == 1) {
+    VertexID_t v = transitEdges[0];
+    if (step > 0) {
+      sample->rootVertices[sample->lastRootIdx] = v;
+    }
+
+    return transitEdges[0];
+  }
+  
+  EdgePos_t x = RandNumGen::rand_int(state, numEdges);
+  VertexID_t v = transitEdges[x];
+
+  if (step > 0) {
+    sample->rootVertices[sample->lastRootIdx] = v;
+  }
+
+  return v;
+}
+
+template<class SampleType, int CACHE_SIZE, bool CACHE_EDGES, bool CACHE_WEIGHTS, bool DECREASE_GM_LOADS>
+__device__ inline
+VertexID nextCached(int step, const VertexID transit, const VertexID sampleIdx, 
+  SampleType* sample,
+              const float max_weight,
+              const CSR::Edge* transitEdges, const float* transitEdgeWeights,
+              const EdgePos_t numEdges, const EdgePos_t neighbrID, 
+              curandState* state, VertexID_t* cachedEdges, float* cachedWeights,
+              bool* globalLoadBV)
+{
+  if (numEdges == 1) {
+    VertexID_t v;
+    if (CACHE_EDGES)
+      v = cacheAndGet<CACHE_SIZE, DECREASE_GM_LOADS>(0, transitEdges, cachedEdges, globalLoadBV);
+    else 
+      v = transitEdges[0];
+    if (step > 0) {
+      sample->rootVertices[sample->lastRootIdx] = v;
+    }
+
+    return v;
+  }
+  
+  EdgePos_t x = RandNumGen::rand_int(state, numEdges);
+  VertexID_t v;
+  if (CACHE_EDGES)
+    v = cacheAndGet<CACHE_SIZE, DECREASE_GM_LOADS>(x, transitEdges, cachedEdges, globalLoadBV);
+  else
+    v = transitEdges[x];
+    
+  if (step > 0) {
+    sample->rootVertices[sample->lastRootIdx] = v;
+  }
+
+  return v;
+}
+
+__host__ int samplingType()
+{
+  return SamplingType::IndividualNeighborhood;
+}
+
+__host__ __device__ OutputFormat outputFormat()
+{
+  return SampledVertices;
+}
+
+__host__ __device__ EdgePos_t numSamples(CSR* graph)
+{
+  return graph->get_n_vertices();
+}
+
+template<class SampleType>
+__host__ std::vector<VertexID_t> initialSample(int sampleIdx, CSR* graph, SampleType& sample)
+{
+  std::vector<VertexID_t> initialValue;
+  initialValue.push_back(sample.rootVertices[0]);
+
+  return initialValue;
+}
+
+__host__ __device__ EdgePos_t initialSampleSize(CSR* graph)
+{
+  return 1;
+}
+
+__host__ __device__ bool hasExplicitTransits()
+{
+  return true;
+}
+
+template<class SampleType>
+__device__ VertexID_t stepTransits(int step, const VertexID_t sampleID, SampleType& sample, int transitIdx, curandState* randState)
+{
+  CSRPartition* csr = (CSRPartition*)&csrPartitionBuff[0];
+  //Use rejection sampling to sample based on the degree of vertices.
+  int x = RandNumGen::rand_int(randState, NUM_ROOT_VERTICES);
+  //printf("x %d\n", x);
+  sample.lastRootIdx = x;
+  return sample.rootVertices[x];
+}
+
+template<class SampleType>
+__host__ SampleType initializeSample(CSR* graph, const VertexID_t sampleID)
+{
+  SampleType sample;
+  //printf("sample %d\n", sampleID);
+  for (int i = 0; i < NUM_ROOT_VERTICES; i++) {
+    sample.rootVertices[i] = i % graph->get_n_vertices();
+    // if (sampleID + i < graph->get_n_vertices()) {
+    //   sample.rootVertices[i] = sampleID + i;
+    // } else {
+    //   sample.rootVertices[i] = sampleID;
+    // }
+  }
+  return sample;
+}
+
+#define RUNS 1
+#define CHECK_RESULTS false
+
+
+template<class SampleType>
+bool checkMultiRWResult(NextDoorData<SampleType>& nextDoorData)
+{
+  //Check result by traversing all sampled neighbors and making
+  //sure that if neighbors at kth-hop is an adjacent vertex of one
+  //of the k-1th hop neighbors.
+  CSR* csr = nextDoorData.csr;
+  auto& initialSamples = nextDoorData.initialContents;
+  auto finalSampleSize = getFinalSampleSize();
+  auto& finalSamples = nextDoorData.hFinalSamples;
+  auto INVALID_VERTEX = nextDoorData.INVALID_VERTEX;
+  auto& samples = nextDoorData.samples;
+  int maxSteps = 4;
+
+  //First create the adjacency matrix.
+  std::cout << "checking results" << std::endl;
+  AdjMatrix adj_matrix;
+
+  csrToAdjMatrix(csr, adj_matrix);
+
+  //Now check the correctness
+  size_t numNeighborsToSampleAtStep = 0;
+  
+  for (int step = 0; step < min(maxSteps, steps()); step++) {
+    if (step == 0) { 
+      bool foundError = false;
+      #pragma omp parallel for shared(foundError)
+      for (size_t s = 0; s < finalSamples.size(); s += finalSampleSize) {
+        std::unordered_set<VertexID_t> uniqueNeighbors;
+        // printf("omp_get_num_threads() %d\n", omp_get_num_threads());
+        const size_t sampleId = s/finalSampleSize;
+        size_t contentsLength = 0;
+        if (stepSize(step) != ALL_NEIGHBORS) {
+          for (size_t v = s + numNeighborsToSampleAtStep; v < s + stepSize(step); v++) {
+            VertexID_t transit = finalSamples[v];
+            uniqueNeighbors.insert(transit);
+            contentsLength += (int)(transit != INVALID_VERTEX);
+
+            bool foundInRootVertices = false;
+            for (int ii = 0; ii < NUM_ROOT_VERTICES; ii++) {
+              VertexID_t rv = samples[sampleId].rootVertices[ii];
+              if (adj_matrix[rv].count(transit) == 1) {
+                foundInRootVertices = true;
+                break;
+              }
+            }
+
+            if (!foundError && transit != INVALID_VERTEX &&
+                !foundInRootVertices) {
+              printf("%s:%d Invalid '%d' in Sample '%ld' at Step '%d'\n", __FILE__, __LINE__, transit, sampleId, step);
+              foundError = true;
+            }
+          }
+
+          if (!foundError && contentsLength == 0) {
+            printf("Step %d: '%ld' vertices sampled for sample '%ld' but sum of edges of all vertices in sample is '%ld'\n", 
+                    step, contentsLength, sampleId, adj_matrix[samples[sampleId].rootVertices[0]].size());
+            foundError = true;
+          }
+        } 
+      }
+
+      if (foundError) return false;
+    } else {
+      bool foundError = false;
+      #pragma omp parallel for shared(foundError)
+      for (size_t s = 0; s < finalSamples.size(); s += finalSampleSize) {
+        const size_t sampleId = s/finalSampleSize;
+        size_t contentsLength = 0;
+        size_t sumEdgesOfNeighborsAtPrevStep = 0;
+        const VertexID_t initialVal = samples[sampleId].rootVertices[0];
+
+        for (size_t v = s + numNeighborsSampledAtStep(step-2); v < s + numNeighborsSampledAtStep(step-1); v++) {
+          sumEdgesOfNeighborsAtPrevStep +=  adj_matrix[finalSamples[v]].size();
+        }
+        
+        // if (sampleId == 48) {
+        //   printf("step %d start %d end %d\n", step, numNeighborsSampledAtStep(step-1),
+        //          ((step == steps() - 1) ? finalSampleSize : numNeighborsSampledAtStep(step)));
+        // }
+        for (size_t v = s + numNeighborsSampledAtStep(step-1); 
+             v < s + ((step == steps() - 1) ? finalSampleSize : numNeighborsSampledAtStep(step)); v++) {
+          VertexID_t transit = finalSamples[v];
+          contentsLength += (int)(transit != INVALID_VERTEX);
+          
+          bool foundInRootVertices = false;
+          for (auto rv : samples[sampleId].rootVertices) {
+            if (adj_matrix[rv].count(transit) == 1) {
+              foundInRootVertices = true;
+              break;
+            }
+          }
+
+          if (transit != INVALID_VERTEX) {
+            if (!foundError && !foundInRootVertices) {
+              printf("%s:%d Invalid '%d' in Sample '%ld' at Step '%d'\n", __FILE__, __LINE__, transit, sampleId, step);
+              std::cout << "Contents of sample : [";
+              for (size_t v2 = s; v2 < s + finalSampleSize; v2++) {
+                std::cout << finalSamples[v2] << ", ";
+              }
+              std::cout << "]" << std::endl;
+              foundError = true;
+            }
+          }
+        }
+
+        if (!foundError && contentsLength == 0 && sumEdgesOfNeighborsAtPrevStep > 0) {
+          printf("Step %d: '%ld' vertices sampled for sample '%ld' but sum of edges of all vertices in sample is '%ld'\n", 
+                  step, contentsLength, sampleId, sumEdgesOfNeighborsAtPrevStep);
+          std::cout << "Contents of sample : [";
+          for (size_t v2 = s; v2 < s + finalSampleSize; v2++) {
+            std::cout << finalSamples[v2] << ", ";
+          }
+          std::cout << "]" << std::endl;
+          foundError = true;
+        }
+      }
+
+      if (foundError) return false;
+    }
+
+    numNeighborsToSampleAtStep = stepSizeAtStep(step);
+  }
+
+  return true;
+}
+
+// APP_TEST(DeepWalk, CiteseerTP, GRAPH_PATH"/citeseer-weighted.graph", 10, false, "TransitParallel") 
+// APP_TEST(DeepWalk, CiteseerSP, GRAPH_PATH"/citeseer-weighted.graph", 10, false, "SampleParallel") 
+// APP_TEST(DeepWalk, MicoTP, GRAPH_PATH"/micro-weighted.graph", 10, false, "TransitParallel")
+// APP_TEST(DeepWalk, MicoSP, GRAPH_PATH"/micro-weighted.graph", 10, false, "SampleParallel") 
+// APP_TEST(DeepWalk, PpiTP, GRAPH_PATH"/ppi_sampled_matrix", 10, false, "TransitParallel")
+// APP_TEST(DeepWalk, PpiSP, GRAPH_PATH"/ppi_sampled_matrix", 10, false, "SampleParallel")
+APP_TEST(MultiRWSample, MultiRW, RedditTP, GRAPH_PATH"/reddit_sampled_matrix", RUNS, CHECK_RESULTS, checkMultiRWResult, "TransitParallel", false)
+APP_TEST(MultiRWSample, MultiRW, RedditLB, GRAPH_PATH"/reddit_sampled_matrix", RUNS, CHECK_RESULTS, checkMultiRWResult, "TransitParallel", true)
+APP_TEST(MultiRWSample, MultiRW, RedditSP, GRAPH_PATH"/reddit_sampled_matrix", RUNS, CHECK_RESULTS, checkMultiRWResult, "SampleParallel", false)
+//APP_TEST(MultiRW, DeepWalk, RedditLB, GRAPH_PATH"/reddit_sampled_matrix", RUNS, CHECK_RESULTS, checkSampledVerticesResult, "TransitParallel", true)
+APP_TEST(MultiRWSample, MultiRW, LiveJournalTP, GRAPH_PATH"/soc-LiveJournal1-weighted.graph", RUNS, CHECK_RESULTS, checkMultiRWResult, "TransitParallel", false)
+APP_TEST(MultiRWSample, MultiRW, LiveJournalLB, GRAPH_PATH"/soc-LiveJournal1-weighted.graph", RUNS, CHECK_RESULTS, checkMultiRWResult, "TransitParallel", true)
+APP_TEST(MultiRWSample, MultiRW, LiveJournalSP, GRAPH_PATH"/soc-LiveJournal1-weighted.graph", RUNS, CHECK_RESULTS, checkMultiRWResult, "SampleParallel", false)
+APP_TEST(MultiRWSample, MultiRW, OrkutTP, GRAPH_PATH"/com-orkut-weighted.graph", RUNS, CHECK_RESULTS, checkMultiRWResult, "TransitParallel", false)
+APP_TEST(MultiRWSample, MultiRW, OrkutLB, GRAPH_PATH"/com-orkut-weighted.graph", RUNS, CHECK_RESULTS, checkMultiRWResult, "TransitParallel", true)
+APP_TEST(MultiRWSample, MultiRW, OrkutSP, GRAPH_PATH"/com-orkut-weighted.graph", RUNS, CHECK_RESULTS, checkMultiRWResult, "SampleParallel", false)
---
+++ src/tests/subGraphSamplingTests.cu
@@ -0,0 +1,204 @@
+#include "testBase.h"
+
+#include <stdlib.h>
+
+#define VERTICES_PER_SAMPLE 64
+
+__host__ __device__ int steps() {return 1;}
+
+__host__ __device__ 
+int stepSize(int k) {
+  return 1;
+}
+
+class SubGraphSample 
+{
+public:
+  VertexID_t vertices[VERTICES_PER_SAMPLE];
+  int adjacencyMatrixLen;
+  int adjacencyMatrixRow[VERTICES_PER_SAMPLE*VERTICES_PER_SAMPLE];
+  int adjacencyMatrixCol[VERTICES_PER_SAMPLE*VERTICES_PER_SAMPLE];
+  int adjacencyMatrixVal[VERTICES_PER_SAMPLE*VERTICES_PER_SAMPLE];
+};
+
+template<class SampleType>
+__device__ inline
+VertexID next(int step, CSRPartition* csr, const VertexID* transits, const VertexID sampleIdx, 
+              SampleType* sample, const float max_weight,
+              const CSR::Edge* transitEdges, const float* transitEdgeWeights,
+              const EdgePos_t numEdges, const EdgePos_t neighbrID, curandState* state)
+{
+  VertexID_t v1 = transits[0];
+  for (int v2Idx = 0; v2Idx < VERTICES_PER_SAMPLE; v2Idx++) {
+    VertexID_t v2 = sample->vertices[v2Idx];
+    bool hasEdge = csr->has_edge_logn(v1, v2);
+    if (hasEdge) {
+      int len = ::atomicAdd(&sample->adjacencyMatrixLen, 1);
+      //int cooIdx = step * NUM_SAMPLED_VERTICES + len;
+      sample->adjacencyMatrixRow[len] = v1;
+      sample->adjacencyMatrixCol[len] = v2;
+      sample->adjacencyMatrixVal[len] = 1.0f;
+    }
+  }
+
+  return -1;
+}
+
+template<class SampleType, int CACHE_SIZE, bool CACHE_EDGES, bool CACHE_WEIGHTS, bool DECREASE_GM_LOADS>
+__device__ inline
+VertexID nextCached(int step, const VertexID transit, const VertexID sampleIdx,
+              SampleType* sample, 
+              const float max_weight,
+              const CSR::Edge* transitEdges, const float* transitEdgeWeights,
+              const EdgePos_t numEdges, const EdgePos_t neighbrID, 
+              curandState* state, VertexID_t* cachedEdges, float* cachedWeights,
+              bool* globalLoadBV)
+{
+  EdgePos_t id = RandNumGen::rand_int(state, numEdges);
+  if (CACHE_EDGES)
+    return cacheAndGet<CACHE_SIZE, DECREASE_GM_LOADS>(id, transitEdges, cachedEdges, globalLoadBV);
+  return transitEdges[id];
+}
+
+__host__ __device__ int samplingType()
+{
+  return SamplingType::IndividualNeighborhood;
+}
+
+__host__ __device__ OutputFormat outputFormat()
+{
+  return AdjacencyMatrix;
+}
+
+__host__ EdgePos_t numSamples(CSR* graph)
+{
+  return graph->get_n_vertices() / VERTICES_PER_SAMPLE / 10;
+}
+
+__host__ __device__ bool hasExplicitTransits()
+{
+  return false;
+}
+
+template<class SampleType>
+__device__ VertexID_t stepTransits(int step, const VertexID_t sampleID, SampleType& sample, int transitIdx, curandState* randState)
+{
+  return -1;
+}
+
+template<class SampleType>
+__host__ std::vector<VertexID_t> initialSample(int sampleIdx, CSR* graph, SampleType& sample)
+{
+  std::vector<VertexID_t> initialValue;
+
+  for (int i = 0; i < VERTICES_PER_SAMPLE; i++) {
+    VertexID_t v = sampleIdx * VERTICES_PER_SAMPLE + i;
+    initialValue.push_back(v);
+    sample.vertices[i] = v;
+  }
+
+  return initialValue;
+}
+
+template<class SampleType>
+__host__ SampleType initializeSample(CSR* graph, const VertexID_t sampleID)
+{
+  SampleType sample;
+  sample.adjacencyMatrixLen = 0;
+
+  return sample;
+}
+
+__host__ __device__ EdgePos_t initialSampleSize(CSR* graph)
+{
+  return VERTICES_PER_SAMPLE;
+}
+
+#define RUNS 1
+#define CHECK_RESULTS true
+
+template<class SampleType>
+bool checkSubGraphResult(NextDoorData<SampleType>& nextDoorData)
+{
+  //Check result by traversing all sampled neighbors and making
+  //sure that if neighbors at kth-hop is an adjacent vertex of one
+  //of the k-1th hop neighbors.
+  CSR* csr = nextDoorData.csr;
+  auto& initialSamples = nextDoorData.initialContents;
+  auto finalSampleSize = getFinalSampleSize();
+  auto& finalSamples = nextDoorData.hFinalSamples;
+  auto INVALID_VERTEX = nextDoorData.INVALID_VERTEX;
+  auto& samples = nextDoorData.samples;
+  int maxSteps = 4;
+
+  //First create the adjacency matrix.
+  std::cout << "checking results" << std::endl;
+  AdjMatrix adj_matrix;
+
+  csrToAdjMatrix(csr, adj_matrix);
+
+  //Now check the correctness
+  size_t numNeighborsToSampleAtStep = 0;
+  bool foundError = false;
+  int sampleIdx = 0;
+  for (SampleType& sample : samples) {
+    //Go through all edges between two vertices and see if they exist in the graph
+    for (int e = 0; e < sample.adjacencyMatrixLen; e++) {
+      VertexID_t v1 = sample.adjacencyMatrixRow[e];
+      VertexID_t v2 = sample.adjacencyMatrixCol[e];
+
+      if (!foundError && adj_matrix[v1].count(v2) == 0) {
+        printf("Sample '%d': no edge '%d' -> '%d' in graph\n", sampleIdx, v1, v2);
+        foundError = true;
+      }
+    }
+
+    //Go through edges between two vertices in graph and see if they exist in sample
+    for (int vidx1 = 0; vidx1 < VERTICES_PER_SAMPLE; vidx1++) {
+      VertexID_t v1 = sample.vertices[vidx1];
+      for (int vidx2 = 0; vidx2 < VERTICES_PER_SAMPLE; vidx2++) {
+        VertexID_t v2 = sample.vertices[vidx2];
+
+        if (adj_matrix[v1].count(v2) == 1 and v1 != v2) {
+          bool foundEdge = false;
+          //Edge in Graph. Check if it is in Sample.
+          for (int e = 0; e < sample.adjacencyMatrixLen; e++) {
+            if (sample.adjacencyMatrixRow[e] == v1 && sample.adjacencyMatrixCol[e] == v2) {
+              foundEdge = true;
+              break;
+            }
+          }
+
+          if (!foundEdge) {
+            if (!foundError) {
+              printf("Sample '%d': Edge '%d'->'%d' exists in graph but not in sample of length '%d'\n", sampleIdx, v1, v2, sample.adjacencyMatrixLen);
+            }
+            foundError = true;
+          }
+        }
+      }
+    }
+
+    sampleIdx++;
+  }
+
+  if (foundError) return false;
+  return true;
+}
+
+// APP_TEST(DeepWalk, CiteseerTP, GRAPH_PATH"/citeseer-weighted.graph", 10, false, "TransitParallel") 
+// APP_TEST(DeepWalk, CiteseerSP, GRAPH_PATH"/citeseer-weighted.graph", 10, false, "SampleParallel") 
+// APP_TEST(DeepWalk, MicoTP, GRAPH_PATH"/micro-weighted.graph", 10, false, "TransitParallel")
+// APP_TEST(DeepWalk, MicoSP, GRAPH_PATH"/micro-weighted.graph", 10, false, "SampleParallel") 
+// APP_TEST(DeepWalk, PpiTP, GRAPH_PATH"/ppi_sampled_matrix", 10, false, "TransitParallel")
+// APP_TEST(DeepWalk, PpiSP, GRAPH_PATH"/ppi_sampled_matrix", 10, false, "SampleParallel")
+APP_TEST(SubGraphSample, SubGraph, RedditTP, GRAPH_PATH"/reddit_sampled_matrix", RUNS, CHECK_RESULTS, checkSubGraphResult, "TransitParallel", false)
+//APP_TEST(SubGraphSample, SubGraph, RedditLB, GRAPH_PATH"/reddit_sampled_matrix", RUNS, CHECK_RESULTS, checkSubGraphResult, "TransitParallel", true)
+APP_TEST(SubGraphSample, SubGraph, RedditSP, GRAPH_PATH"/reddit_sampled_matrix", RUNS, CHECK_RESULTS, checkSubGraphResult, "SampleParallel", false)
+//APP_TEST(SubGraph, DeepWalk, RedditLB, GRAPH_PATH"/reddit_sampled_matrix", RUNS, CHECK_RESULTS, checkSampledVerticesResult, "TransitParallel", true)
+//APP_TEST(SubGraphSample, SubGraph, LiveJournalTP, GRAPH_PATH"/soc-LiveJournal1-weighted.graph", RUNS, CHECK_RESULTS, checkSubGraphResult, "TransitParallel", false)
+//APP_TEST(SubGraphSample, SubGraph, LiveJournalLB, GRAPH_PATH"/soc-LiveJournal1-weighted.graph", RUNS, CHECK_RESULTS, checkSubGraphResult, "TransitParallel", true)
+APP_TEST(SubGraphSample, SubGraph, LiveJournalSP, GRAPH_PATH"/soc-LiveJournal1-weighted.graph", RUNS, CHECK_RESULTS, checkSubGraphResult, "SampleParallel", false)
+//APP_TEST(SubGraphSample, SubGraph, OrkutTP, GRAPH_PATH"/com-orkut-weighted.graph", RUNS, CHECK_RESULTS, checkSubGraphResult, "TransitParallel", false)
+//APP_TEST(SubGraphSample, SubGraph, OrkutLB, GRAPH_PATH"/com-orkut-weighted.graph", RUNS, CHECK_RESULTS, checkSubGraphResult, "TransitParallel", true)
+APP_TEST(SubGraphSample, SubGraph, OrkutSP, GRAPH_PATH"/com-orkut-weighted.graph", RUNS, CHECK_RESULTS, checkSubGraphResult, "SampleParallel", false)
