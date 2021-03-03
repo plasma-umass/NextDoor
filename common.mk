5dcb609187ee8218faf70c563d8adb94bbec265d
Ubuntu <ubuntu@ip-172-31-53-163.ec2.internal>
2021-03-03 21:05:51 +0000

    update gencode

--- common.mk
+++ common.mk
@@ -1,3 +1,3 @@
-ARCH_CODE_FLAGS = -arch=compute_61 -code=sm_61
+ARCH_CODE_FLAGS = -gencode arch=compute_61,code=sm_61 -gencode arch=compute_70,code=sm_70 -gencode arch=compute_52,code=sm_52
 PYTHON_2_INCLUDE_PATH = /usr/include/python2.7/
 PYTHON_3_INCLUDE_PATH = /usr/include/python3.6m/
