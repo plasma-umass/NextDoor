fcb16a8260a3117fa9596293d0e00ed70ec3b7f5
Abhinav Jangda <abhijangda@gmail.com>
2022-03-16 20:22:57 -0400

    compilation flags for Ampere

--- common.mk
+++ common.mk
@@ -1,3 +1,3 @@
-ARCH_CODE_FLAGS = -gencode arch=compute_61,code=sm_61 -gencode arch=compute_70,code=sm_70 -gencode arch=compute_37,code=sm_37 -gencode arch=compute_35,code=sm_35 -gencode arch=compute_50,code=sm_50 -gencode arch=compute_52,code=sm_52
+ARCH_CODE_FLAGS = -gencode arch=compute_80,code=sm_80 -gencode arch=compute_61,code=sm_61 -gencode arch=compute_70,code=sm_70
 PYTHON_2_INCLUDE_PATH = /usr/include/python2.7/
 PYTHON_3_INCLUDE_PATH = /usr/include/python3.6m/ -I/usr/include/python3.8/ -I/usr/include/python3.7/
